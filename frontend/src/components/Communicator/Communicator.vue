7251976391439adc335ee39fc671d1b084eebb9e
Nyrod <pawfr876@gmail.com>
2017-11-26 13:11:23 +0100

    Strona komunikatora i profilu uzytkownika

--- frontend/src/components/Communicator/Communicator.vue
+++ frontend/src/components/Communicator/Communicator.vue
@@ -1,6 +1,139 @@
 <template>
-  <div>
-    Communicator page
-  </div>
+  <v-container>
+    <v-layout column>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-title>
+            <div>Chat with </div>
+            <v-chip disabled>
+              <v-avatar>
+               <img src="https://randomuser.me/api/portraits/men/35.jpg" alt="Chat with">
+              </v-avatar>
+              Imie i nazwisko
+            </v-chip>
+          </v-card-title>
+          <v-divider></v-divider>
+          <v-card-text>
+            <v-container
+              style="max-height: 400px"
+              class="scroll-y">
+              <v-layout row>
+                <v-flex>
+                  <div>
+                    <v-chip disabled small>
+                      sadas
+                    </v-chip>
+                  </div>
+                  <div style="text-align: right;">
+                    <v-chip disabled small>
+                      dsadsaa
+                    </v-chip>
+                  </div>
+                  <div>
+                    <v-chip disabled small>
+                      sadas
+                    </v-chip>
+                  </div>
+                  <div style="text-align: right;">
+                    <v-chip disabled small>
+                      dsadsaa
+                    </v-chip>
+                  </div>
+                  <div>
+                    <v-chip disabled small>
+                      sadas
+                    </v-chip>
+                  </div>
+                  <div style="text-align: right;">
+                    <v-chip disabled small>
+                      dsadsaa
+                    </v-chip>
+                  </div>
+                  <div>
+                    <v-chip disabled small>
+                      sadas
+                    </v-chip>
+                  </div>
+                  <div style="text-align: right;">
+                    <v-chip disabled small>
+                      dsadsaa
+                    </v-chip>
+                  </div><div>
+                  <v-chip disabled small>
+                    sadas
+                  </v-chip>
+                </div>
+                  <div style="text-align: right;">
+                    <v-chip disabled small>
+                      dsadsaa
+                    </v-chip>
+                  </div>
+                  <div>
+                    <v-chip disabled small>
+                      sadas
+                    </v-chip>
+                  </div>
+                  <div style="text-align: right;">
+                    <v-chip disabled small>
+                      dsadsaa
+                    </v-chip>
+                  </div>
+                  <div>
+                    <v-chip disabled small>
+                      sadas
+                    </v-chip>
+                  </div>
+                  <div style="text-align: right;">
+                    <v-chip disabled small>
+                      dsadsaa
+                    </v-chip>
+                  </div>
+                  <div>
+                    <v-chip disabled small>
+                      sadas
+                    </v-chip>
+                  </div>
+                  <div style="text-align: right;">
+                    <v-chip disabled small>
+                      dsadsaa
+                    </v-chip>
+                  </div>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-text-field
+              v-model="message"
+              label="Enter message"
+              multi-line
+              rows="1"
+              auto-grow
+              elevation-22>
+            </v-text-field>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3>
+        <!--<div class="chat-container">-->
+        <!--<message :messages="messages" @imageLoad="scrollToEnd"></message>-->
+        <!--</div>-->
+        <!--<emoji-picker :show="emojiPanel" @close="toggleEmojiPanel" @click="addMessage"></emoji-picker>-->
+        <!--<div class="typer">-->
+        <!--<input type="text" placeholder="Enter message" v-on:keyup.enter="sendMessage" v-model="content" elevation-22>-->
+        <!--</div>-->
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
+<script>
+  export default {
+    data () {
+      return {
+        message: null
+      }
+    }
+  }
+</script>
--- frontend/src/components/User/Profile.vue
+++ frontend/src/components/User/Profile.vue
@@ -1,8 +1,37 @@
 <template>
   <v-container>
-    <v-layout row>
-      <v-flex xs12  sm6 offset-sm3>
-
+    <v-layout row wrap>
+      <v-flex xs12 sm6 text-xs-center text-sm-left>
+        <v-avatar size="100px">
+          <img src="http://www.kenia1100.pl/wp-content/uploads/2016/07/pikatchu-pokemon-300x300.png" alt="avatar">
+        </v-avatar>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;IMIE I NAZWISKO
+      </v-flex>
+      <v-flex xs12 sm6 text-xs-center text-sm-right>
+        <v-btn flat>
+          <v-icon left>mode_edit</v-icon>
+          Edit
+        </v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs12 text-xs-center mt-4>
+        <v-text-field
+          disabled
+          v-model="description"
+          label="Description"
+          multi-line
+          rows="1"
+          auto-grow
+          elevation-22>
+        </v-text-field>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12 text-xs-center mt-4>
+        <v-carousel>
+          <v-carousel-item v-for="(item,i) in items" v-bind:src="item.src" :key="i"></v-carousel-item>
+        </v-carousel>
       </v-flex>
     </v-layout>
   </v-container>
@@ -12,7 +41,15 @@
   export default {
     data () {
       return {
-
+        description: 'sfasasdfas',
+        items: [
+          {
+            src: 'http://www.kenia1100.pl/wp-content/uploads/2016/07/pikatchu-pokemon-300x300.png'
+          },
+          {
+            src: 'http://segritta.pl/wp-content/uploads/2016/08/landscape-1456483171-pokemon2.jpg'
+          }
+        ]
       }
     }
   }
