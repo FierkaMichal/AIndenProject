caf4bba3a2e0266e1796d1dc813f14e26a30a4b0
Michał <fierkamichal@gmail.com>
2017-11-28 16:46:21 +0100

    fix

--- backend/src/main/java/com/ainder/ainder/controllers/AccountController.java
+++ backend/src/main/java/com/ainder/ainder/controllers/AccountController.java
@@ -51,7 +51,7 @@ public class AccountController {
             return new ResponseEntity<>(new Error("UserResponse with that login already exists."), HttpStatus.CONFLICT);
         }
 
-        userService.save(new User(0l, registration.getName(), registration.getSurname(), registration.getLogin(), registration.getPassword(), 0d,0d,roleService.getRoleById(2l)));
+        userService.save(new User(0l, registration.getName(), registration.getSurname(), registration.getLogin(), registration.getPassword(), 0d,0d,roleService.getRoleById(1l)));
 
         return new ResponseEntity<>(new Error(), HttpStatus.OK);
     }
