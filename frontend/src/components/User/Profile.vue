0242077393245acec324d5b5399840f8b25d3701
Nyrod <pawfr876@gmail.com>
2018-01-03 17:24:23 +0100

    Wyswietlanie avatar

--- frontend/src/components/User/Profile.vue
+++ frontend/src/components/User/Profile.vue
@@ -82,7 +82,7 @@
                 </v-layout>
                 <v-layout row>
                   <v-flex text-xs-center>
-                    <img :src="avatarSelected" height="250">
+                    <img :src="avatarSelected" height="250" alt="avatar">
                   </v-flex>
                 </v-layout>
                 <v-divider></v-divider>
@@ -95,7 +95,7 @@
                 </v-layout>
                 <v-layout row>
                   <v-flex text-xs-center>
-                    <img :src="photoSelected" height="250" alt="avatar">
+                    <img :src="photoSelected" height="250" alt="photo">
                   </v-flex>
                 </v-layout>
                 <v-divider></v-divider>
